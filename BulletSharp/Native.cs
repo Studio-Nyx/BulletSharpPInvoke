03532e982bf42c98552b99bd5747d7cad760f89b
jeremyBois <jeremy.bois@studio-nyx.com>
2023-11-22 10:45:03 +0100

    🔨 🏗️ [Bullet DLL] - Cpp bullet library is now selected based on build target.

--- BulletSharp/Native.cs
+++ BulletSharp/Native.cs
@@ -4,7 +4,12 @@ namespace BulletSharp
 {
     public static class Native
     {
+#if DEBUG
+        public const string Dll = "libbulletc_Debug";
+#else
         public const string Dll = "libbulletc";
+#endif
+
         public const CallingConvention Conv = CallingConvention.Cdecl;
     }
 }
